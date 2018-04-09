bf08ba9a6f03f3449f46ab70999f5abca42372f1
Fabio Zuin <fabiozuin@outlook.com>
2018-04-08 21:51:57 -0300

    UPDATE DOCUMENT

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var express = require('express'),
 var port = 8080;
 var Cloudant = require('@cloudant/cloudant');
 var cloudant = Cloudant(
-'https://09409268-3578-40b1-87f8-0d854a4d17b8-bluemix:896f65c85ee46d141e262b7fb20fbe94fdc8aee046d4d5f75c04ca6db85da6dc@09409268-3578-40b1-87f8-0d854a4d17b8-bluemix.cloudant.com'  
+  'https://09409268-3578-40b1-87f8-0d854a4d17b8-bluemix:896f65c85ee46d141e262b7fb20fbe94fdc8aee046d4d5f75c04ca6db85da6dc@09409268-3578-40b1-87f8-0d854a4d17b8-bluemix.cloudant.com'
 );
 var watson = require('watson-developer-cloud');
 var formidable = require('formidable');
@@ -14,7 +14,7 @@ var conversation = new watson.ConversationV1({
   version_date: '2018-02-16'
 });
 
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
@@ -48,12 +48,12 @@ pdfText(pathToPdf, function(err, chunks) {
 
 app.use(bodyParser.json());
 
-app.post('/train/pdf', function(req, res) {
+app.post('/train/pdf', function (req, res) {
   var form = new formidable.IncomingForm();
-  form.parse(req, function(err, fields, files) {
+  form.parse(req, function (err, fields, files) {
     var oldpath = files.path;
     var newpath = files.name;
-    fs.rename(oldpath, newpath, function(err) {
+    fs.rename(oldpath, newpath, function (err) {
       if (err) throw err;
       res.write('File uploaded and moved!');
       res.end();
@@ -61,8 +61,8 @@ app.post('/train/pdf', function(req, res) {
   });
 });
 
-app.post('/createuser', function(req, res) {
-  education.insert({ userId: 1 }, function(err, body, header) {
+app.post('/createuser', function (req, res) {
+  education.insert({ userId: 1 }, function (err, body, header) {
     if (err) {
       res.status(500).send({ message: "Can't create User" });
     }
@@ -75,18 +75,18 @@ app.post('/createuser', function(req, res) {
 * This create the chat bot and the course on the backend database.
 */
 
-app.post('/newcourse', function(req, res) {
+app.post('/newcourse', function (req, res) {
   var course = {
     name: req.body.name,
     description: req.body.description,
     lessons_number: req.body.lessons_number,
     createdBy: req.body.createdBy
   };
-  insertIntoDatabase(course).then(function(result_database) {
+  insertIntoDatabase(course).then(function (result_database) {
     if (result_database.message) {
       res.status(500).send(result_database);
     } else {
-      createChatBot(result_database.id, req.body.description).then(function(
+      createChatBot(result_database.id, req.body.description).then(function (
         result_chatbot
       ) {
         if (result_chatbot.error) {
@@ -99,26 +99,25 @@ app.post('/newcourse', function(req, res) {
   });
 });
 
-//WE NEED TO UPDATE THIS EVERYTIME WE CHANGE A STEP;
-app.post('/updatecourse', function(req, res) {
+app.post('/updatecourse', function (req, res) {
   console.log(req.body);
-  req.body._id = req.body.id;
-  req.body._rev = Math.random();
-  console.log("Updating document 'mydoc'");
-  // make a change to the document, using the copy we kept from reading it back
-  education.insert(req.body, function(err, data) {
-    console.log('Error:', err);
-    console.log('Data:', data);
-    // keep the revision of the update so we can delete it
-    res.sendStatus(200);
-  
+  education.get(req.body.id, function (err, dataGet) {
+    req.body._rev = dataGet._rev
+    req.body._id = req.body._id
+
+    console.log("Updating document 'mydoc'");
+    education.insert(req.body, req.body.id, function (err, data) {
+      console.log('Error:', err);
+      console.log('Data:', data);
+      res.sendStatus(200);
+
+    });
   });
-  
 });
 
 function insertIntoDatabase(document) {
-  return new Promise(function(resolve, reject) {
-    education.insert(document, function(err, body, header) {
+  return new Promise(function (resolve, reject) {
+    education.insert(document, function (err, body, header) {
       if (err) {
         reject(err);
         console.log("Can't insert into DB Error:", err.message);
@@ -133,8 +132,8 @@ function createChatBot(databaseId, description) {
     name: 'course_' + databaseId,
     description: description
   };
-  return new Promise(function(resolve, reject) {
-    conversation.createWorkspace(workspace, function(err, response) {
+  return new Promise(function (resolve, reject) {
+    conversation.createWorkspace(workspace, function (err, response) {
       if (err) {
         var error = { error: "Can't create the workspace" };
         reject(error);
