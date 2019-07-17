149ca4e8d745bc70f3eefb054ab17cbb52001cd1
x221078 <p000fabio.zunin@prservicos.com.br>
2019-07-17 15:24:45 -0300

    qualquer

--- pdfML.js
+++ pdfML.js
@@ -24,41 +24,79 @@ class pdfML {
     });
 
     // Train and test:
-    this.generateTrainBatch = function(){
-      var arrayTrain =[];
-      for(let i=0; i<= 1000; i++){
-        arrayTrain.push({input: i.toString(), output: 'normal_number'});
-        arrayTrain.push({input: i.toString() + '-', output: 'question_start'});
-        arrayTrain.push({input: i.toString()+ '-)', output: 'question_start'});
-        arrayTrain.push({input: i.toString()+ '.', output: 'question_start'});
-        arrayTrain.push({input: i.toString()+ '.)', output: 'question_start'});
-        arrayTrain.push({input: i.toString()+ ')', output: 'question_start'});
+    this.generateTrainBatch = function() {
+      var arrayTrain = [];
+      for (let i = 0; i <= 1000; i++) {
+        arrayTrain.push({ input: i.toString(), output: 'normal_number' });
+        arrayTrain.push({
+          input: i.toString() + '-',
+          output: 'question_start'
+        });
+        arrayTrain.push({
+          input: i.toString() + '-)',
+          output: 'question_start'
+        });
+        arrayTrain.push({
+          input: i.toString() + '.',
+          output: 'question_start'
+        });
+        arrayTrain.push({
+          input: i.toString() + '.)',
+          output: 'question_start'
+        });
+        arrayTrain.push({
+          input: i.toString() + ')',
+          output: 'question_start'
+        });
       }
 
       //EXAMPLES OF TRAINING FOR QUIESTIONS BODY
-      arrayTrain.push({input: 'abcdefghijklmnopqrstuvywz', output: 'question_body'})
-      arrayTrain.push({input: 'De acordo com o texto, o que podemos observar nessa foto', output: 'question_body'})
-      arrayTrain.push({input: 'Qual seu pai', output: 'question_body'})
-      arrayTrain.push({input: 'Qual algo ', output: 'question_body'})
-      arrayTrain.push({input: 'Qual seria', output: 'question_body'})
-      arrayTrain.push({input: 'Por que será que existem essas normas? Escolha a alternativa que melhor justifica essas normas', output: 'question_body'})
-      arrayTrain.push({input: 'Vamos fazer um exercício. Pegue um mapa do Brasil e tente descobrir que estado não fazia parte do território brasileiro no século XVIII', output: 'question_body'})
-      arrayTrain.push({input: 'Analisando o Gráfico, é possível observar que a população das cidades passou a ser maior que a do campo', output: 'question_body'})
-      arrayTrain.push({input: 'De acordo com o texto, o que torna a água motivo de conflito é o fato de', output: 'question_body'})
-      
+      arrayTrain.push({
+        input: 'abcdefghijklmnopqrstuvywz',
+        output: 'question_body'
+      });
+      arrayTrain.push({
+        input: 'De acordo com o texto, o que podemos observar nessa foto',
+        output: 'question_body'
+      });
+      arrayTrain.push({ input: 'Qual seu pai', output: 'question_body' });
+      arrayTrain.push({ input: 'Qual algo ', output: 'question_body' });
+      arrayTrain.push({ input: 'Qual seria', output: 'question_body' });
+      arrayTrain.push({
+        input:
+          'Por que será que existem essas normas? Escolha a alternativa que melhor justifica essas normas',
+        output: 'question_body'
+      });
+      arrayTrain.push({
+        input:
+          'Vamos fazer um exercício. Pegue um mapa do Brasil e tente descobrir que estado não fazia parte do território brasileiro no século XVIII',
+        output: 'question_body'
+      });
+      arrayTrain.push({
+        input:
+          'Analisando o Gráfico, é possível observar que a população das cidades passou a ser maior que a do campo',
+        output: 'question_body'
+      });
+      arrayTrain.push({
+        input:
+          'De acordo com o texto, o que torna a água motivo de conflito é o fato de',
+        output: 'question_body'
+      });
+
+      arrayTrain.push({ input: 'R:', output: 'question_answer' });
+      arrayTrain.push({ input: 'R.', output: 'question_answer' });
+
       return arrayTrain;
-    }
+    };
 
     this.arrayOfTraining = this.generateTrainBatch();
-    this.intentClassifier.trainBatch(
-     this.arrayOfTraining
-    );
+    this.intentClassifier.trainBatch(this.arrayOfTraining);
 
     console.dir(this.intentClassifier.classify('DEPENDE DE ALGO OU NAO'));
   }
 
   analyzeText(text) {
-    console.log(this.intentClassifier.classify(text))
+    console.log(this.intentClassifier.classify(text));
   }
 }
 module.exports = pdfML;
--- server.js
+++ server.js
@@ -2,147 +2,21 @@ var bodyParser = require('body-parser');
 var express = require('express'),
   app = express();
 var port = 8080;
-var Cloudant = require('@cloudant/cloudant');
-var cloudant = Cloudant(
-  'https://09409268-3578-40b1-87f8-0d854a4d17b8-bluemix:896f65c85ee46d141e262b7fb20fbe94fdc8aee046d4d5f75c04ca6db85da6dc@09409268-3578-40b1-87f8-0d854a4d17b8-bluemix.cloudant.com'
-);
-var watson = require('watson-developer-cloud');
-var formidable = require('formidable');
-var conversation = new watson.ConversationV1({
-  username: '5fe0767b-af08-4def-8f36-4605eacf7248',
-  password: 'rr1K2zW1q8kq',
-  version_date: '2018-02-16'
-});
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
   next();
 });
 
-var education = cloudant.db.use('education');
-// use session ref to call API, i.e.:
-
-var pdfText = require('pdf-text');
-
-var pathToPdf = 'que.pdf';
-app.use(
-  bodyParser.urlencoded({
-    extended: false
-  })
-);
-
-
-//var pdfML = require('./pdfML');
-//var pdfAnalyzer = new pdfML();
-
-/*
-pdfText(pathToPdf, function(err, chunks) {
-  console.log(chunks);
-  for (let i=0; i<chunks.length; i++){
-    console.log(chunks[i]);
-    pdfAnalyzer.analyzeText(chunks[i]);
-  }
-}); */
-
-
 app.use(bodyParser.json());
 
-app.post('/train/pdf', function (req, res) {
-  var form = new formidable.IncomingForm();
-  form.parse(req, function (err, fields, files) {
-    var oldpath = files.path;
-    var newpath = files.name;
-    fs.rename(oldpath, newpath, function (err) {
-      if (err) throw err;
-      res.write('File uploaded and moved!');
-      res.end();
-    });
-  });
+app.get('/domain/maritalstatus', function(req, res) {
+  res.send({});
 });
-
-app.post('/createuser', function (req, res) {
-  education.insert({ userId: 1 }, function (err, body, header) {
-    if (err) {
-      res.status(500).send({ message: "Can't create User" });
-    }
-    res.send({ message: 'User created with success' });
-    console.log(body);
-  });
-});
-
-/*
-* This create the chat bot and the course on the backend database.
-*/
-
-app.post('/newcourse', function (req, res) {
-  var course = {
-    name: req.body.name,
-    description: req.body.description,
-    lessons_number: req.body.lessons_number,
-    createdBy: req.body.createdBy
-  };
-  insertIntoDatabase(course).then(function (result_database) {
-    if (result_database.message) {
-      res.status(500).send(result_database);
-    } else {
-      createChatBot(result_database.id, req.body.description).then(function (
-        result_chatbot
-      ) {
-        if (result_chatbot.error) {
-          res.status(500).send(result_chatbot);
-        } else {
-          res.status(200).send(result_database);
-        }
-      });
-    }
-  });
-});
-
-app.post('/updatecourse', function (req, res) {
-  console.log(req.body);
-  education.get(req.body.id, function (err, dataGet) {
-    req.body._rev = dataGet._rev
-    req.body._id = req.body._id
-
-    console.log("Updating document 'mydoc'");
-    education.insert(req.body, req.body.id, function (err, data) {
-      console.log('Error:', err);
-      console.log('Data:', data);
-      res.sendStatus(200);
-
-    });
-  });
-});
-
-function insertIntoDatabase(document) {
-  return new Promise(function (resolve, reject) {
-    education.insert(document, function (err, body, header) {
-      if (err) {
-        reject(err);
-        console.log("Can't insert into DB Error:", err.message);
-      }
-      resolve(body);
-    });
-  });
-}
-
-function createChatBot(databaseId, description) {
-  var workspace = {
-    name: 'course_' + databaseId,
-    description: description
-  };
-  return new Promise(function (resolve, reject) {
-    conversation.createWorkspace(workspace, function (err, response) {
-      if (err) {
-        var error = { error: "Can't create the workspace" };
-        reject(error);
-      } else {
-        resolve(JSON.stringify(response, null, 2));
-      }
-    });
-  });
-}
 
 app.use(express.static(__dirname + '/public'));
 
