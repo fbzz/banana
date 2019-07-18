cd9adf270a62b3891c35e82a1b2c6eb5a3e55053
x221078 <p000fabio.zunin@prservicos.com.br>
2019-07-18 09:40:00 -0300

    MAIS COISA

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
+'use strict';
+
 var bodyParser = require('body-parser');
 var express = require('express'),
   app = express();
-var port = process.env.PORT || 8080;
-
-app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+var port = process.env.ENV || 8080;
 
 app.use(bodyParser.json());
 
+
 app.use(express.static(__dirname + '/public'));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
 
 app.listen(port);
 console.log('Listening on port ', port);
